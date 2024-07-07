ffe174238f35fecacbcefbccbbcfb92405174811
d4ve10 <davegiwo@gmail.com>
2024-07-07 21:12:20 +0200

    fixed OpenMP include

--- before.cpp
+++ before.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 
   double t0 = omp_get_wtime(); // start time
 
-  #pragma omp parallel for
+  // #pragma omp parallel for collapse(2)
   for (int i = 0; i < image.rows; ++i) {
     for (int j = 0; j < image.cols; ++j) {
 
@@ -26,6 +26,7 @@ int main(int argc, char** argv)
       float B = pixel[0] / 255.0f;
       float G = pixel[1] / 255.0f;
       float R = pixel[2] / 255.0f;
+      // std::cout << "threadId: " << omp_get_thread_num() << std::endl;
 
       // Vorlesung:
       float y = 16 + (65.481f * R + 128.553f * G + 24.966f * B);
